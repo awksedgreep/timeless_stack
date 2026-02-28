3d6d2ef67a0e9104395ac17ce5d8ad63af81ec1a
Mark Cotner <mark.cotner@gmail.com>
2026-02-28 18:22:01 -0500

    bump version to 0.3.4, update ex_openzl to ~> 0.4.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TimelessStack.MixProject do
   def project do
     [
       app: :timeless_stack,
-      version: "0.3.3",
+      version: "0.3.4",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -35,7 +35,7 @@ defmodule TimelessStack.MixProject do
       {:timeless_metrics, path: "../timeless_metrics"},
       {:timeless_logs, github: "awksedgreep/timeless_logs"},
       {:timeless_traces, github: "awksedgreep/timeless_traces"},
-      {:ex_openzl, "~> 0.4.0", override: true},
+      {:ex_openzl, "~> 0.4.5", override: true},
       {:timeless_ui, path: "../timeless_ui"},
       {:hackney, "~> 1.20"}
     ]
